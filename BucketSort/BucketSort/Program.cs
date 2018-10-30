aeeab075e28a471f90095a7d69d49788cca18040
endoffile78 <endoffile78@users.noreply.github.com>
2018-10-30 15:50:00 -0500

    Add input and create buckets

--- BucketSort/BucketSort/Program.cs
+++ BucketSort/BucketSort/Program.cs
@@ -9,14 +9,26 @@ namespace BucketSort
     class Program
     {
 
-        static void bucketSort(ref int[] arr) 
+        public static void bucketSort(ref int[] arr) 
         {
-
+            List<int>[] buckets = new List<int>[arr.Length];
         }
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter numbers seperated by spaces: ");
+            string input = Console.ReadLine().Trim();
+            string[] numbers = input.Split(' ');
 
+            int[] arr = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++) {
+                int num;
+                bool ret = int.TryParse(numbers[i].Trim(), out num);
+                if (!ret) {
+                    continue;
+                }
+                arr[i] = num;
+            }
         }
     }
 }
