a0970df362ac826742b83aa358260d1c54d2927f
endoffile78 <endoffile78@users.noreply.github.com>
2018-10-30 22:32:00 -0500

    Finish the bucket sort function

--- BucketSort/BucketSort/Program.cs
+++ BucketSort/BucketSort/Program.cs
@@ -4,23 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BucketSort
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace BucketSort {
+    class Program {
+        static void Main(string[] args) {
             //Beginning of the user input section, takes the user input into an array while trimming extra space of the end
             Console.WriteLine("Please enter numbers seperated by spaces: ");
             string input = Console.ReadLine().Trim();
             string[] numbers = input.Split(' ');
             int num = 0;
             int[] arr = new int[numbers.Length];
-            for (int i = 0; i < numbers.Length; i++)
-            {
+            for (int i = 0; i < numbers.Length; i++) {
                 bool ret = int.TryParse(numbers[i].Trim(), out num);
-                if (!ret)
-                { //Isn't a number, unable to parse it, so then it just skips it
+                if (!ret) { //Isn't a number, unable to parse it, so then it just skips it
                     Console.WriteLine("[ERROR] Can not parse: " + numbers[i] + " Skipping...");
                     continue;
                 }
@@ -35,38 +30,29 @@ namespace BucketSort
             printArray(arr); // Prints out sorted array
         }
 
-        public static void bucketSort(ref int[] arr) 
-        {
-            List<int>[] buckets = new List<int>[arr.Length - 1];
-            for (int i = 0; i < buckets.Length; i++) { // initialize all of the lists
-                buckets[i] = new List<int>();
-            }
-
+        public static void bucketSort(ref int[] arr) {
             int max = arr.Max();
-            int divider = (max + 1)/buckets.Length;
 
-            for (int j = 0; j < arr.Length; j++) {
-                int index = arr[j] % buckets.Length;
-                buckets[index].Add(arr[j]);
+            int[] buckets = new int[max + 1]; //create an array big enough to hold all numbers
+
+            for (int i = 0; i < arr.Length; i++) {
+                buckets[arr[i]]++;
             }
 
             int position = 0;
             for (int i = 0; i < buckets.Length; i++) {
-                if (buckets[i].Count > 0) {
-                    int[] list = buckets[i].ToArray();
-                    for (int j = 0; j < list.Length; j++) {
-                        arr[position] = list[j];
-                        position++;
-                    }
+                for (int j = 0; j < buckets[i]; j++) {
+                    Console.WriteLine(i);
+                    arr[position] = i; //put the number into the new spot
+                    position++;
                 }
             }
         }
+
         // This is the function that will print the array. 
-        public static void printArray(int[] x)
-        {
+        public static void printArray(int[] x) {
             //runs through a for loop to see how long the array is, when it gets to the last variable it starts.
-            for (int i = 0; i < x.Length; i++) 
-            {
+            for (int i = 0; i < x.Length; i++) {
                 //Writes the array
                 Console.Write("[" + x[i] + "] ");
             }
